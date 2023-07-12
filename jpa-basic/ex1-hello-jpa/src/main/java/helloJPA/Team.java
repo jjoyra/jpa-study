f8b623837e5a244f3197763704924465fd15ab52
jjoyra <bunny7531@naver.com>
2023-07-12 23:38:29 +0900

    [0717] jpa-basic : 05. 연관관계 매핑 기초 ing

--- jpa-basic/ex1-hello-jpa/src/main/java/helloJPA/Team.java
+++ jpa-basic/ex1-hello-jpa/src/main/java/helloJPA/Team.java
@@ -1,9 +1,8 @@
 package helloJPA;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -12,19 +11,6 @@ public class Team {
     private Long id;
     private String name;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    @OneToMany(mappedBy = "team")
+    private List<Member> memebers = new ArrayList<>();
 }
