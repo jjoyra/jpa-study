b7361c9dcf28b3b5dd07d70686645e2640e6f8a4
jjoyra <bunny7531@naver.com>
2023-07-18 00:01:55 +0900

    [0717] jpa-basic : 07. 고급 매핑

---
+++ jpa-basic/jpashop/src/main/java/jpabook/jpashop/domain/BaseEntity.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    private String createBy;
+    private LocalDateTime cratedDate;
+    private String lastModifiedBy;
+    private LocalDateTime lastModifiedDate;
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public LocalDateTime getCratedDate() {
+        return cratedDate;
+    }
+
+    public void setCratedDate(LocalDateTime cratedDate) {
+        this.cratedDate = cratedDate;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public LocalDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+}
