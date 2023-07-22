2b29956dc1ffee26a11eb1033bdae3f05403e5f7
jjoyra <bunny7531@naver.com>
2023-07-22 23:46:58 +0900

    [0722] jpa-basic : 08.프록시와 연관관계 관리

--- jpa-basic/jpashop/src/main/java/jpabook/jpashop/domain/Category.java
+++ jpa-basic/jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -4,6 +4,8 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 public class Category extends BaseEntity {
 
@@ -11,7 +13,7 @@ public class Category extends BaseEntity {
     private Long Id;
     private String name;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "PARENT_ID")
     private Category parent;
 
@@ -24,4 +26,7 @@ public class Category extends BaseEntity {
             inverseJoinColumns = @JoinColumn(name="ITEM_ID")
     )
     private List<Item> items = new ArrayList<>();
+
+    public Category() {
+    }
 }
